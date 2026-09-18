3b1f4a867b0358a33fdbd9c3cdd00d6c7e13c531
Nicholas Penree <nick@penree.com>
2026-09-18 17:23:43 -0400

    style: format web zones

--- internal/web/zones.go
+++ internal/web/zones.go
@@ -96,7 +96,7 @@ func (server *Server) zonesView(request *http.Request, message, errorMessage, se
 			ID:   zone.ID,
 			Name: zone.Name, Type: zone.Type, DefaultTTL: zone.DefaultTTL, Disabled: zone.Disabled,
 			ShowFullRecordNames: console.ShowFullRecordNames,
-			ZoneTransfer: zone.ZoneTransfer, TransferACL: append([]string(nil), zone.TransferACL...), Notify: append([]string(nil), zone.Notify...),
+			ZoneTransfer:        zone.ZoneTransfer, TransferACL: append([]string(nil), zone.TransferACL...), Notify: append([]string(nil), zone.Notify...),
 			PrimaryServers: append([]string(nil), zone.PrimaryServers...), PrimaryProtocol: zone.PrimaryProtocol,
 			AliasZone: zone.AliasZone, AliasSources: aliasZoneChoices(aliasSources, zone),
 			CatalogZone: zone.CatalogZone, CatalogGroup: zone.CatalogGroup, CatalogMemberID: zone.CatalogMemberID,
